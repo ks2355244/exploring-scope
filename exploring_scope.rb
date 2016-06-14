113993ac8d0ac1391ec6d969446c7f7e99f2c73c
ks2355244 <ks2355244@yahoo.com>
2016-06-14 11:23:50 +0800

    1st commit

--- exploring_scope.rb
+++ exploring_scope.rb
@@ -1,1 +1,23 @@
 # Place your solutions here
+# class GlobalScope
+# 	$get_local_var
+# 	$local_var
+
+class BasicClass
+	
+	def initialize
+		local_var = 5
+		@instance_var = 3
+	end
+
+	def get_local_var
+		return local_var 
+	end
+
+	def get_instance_var
+		return @instance_var
+	end
+end
+
+var = BasicClass.new
+puts var.get_instance_var 
